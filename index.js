8873921d5f422b9071089c59a2e2ab905ef194c7
Anthematics <jasonharder2@gmail.com>
2018-02-05 20:40:23 -0500

    got form information to save but not persist yet in local storage

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello! I am loaded');
 
-
   const button = document.querySelector('.clickme');
   button.addEventListener('click', () => {
-    console.log('clicked');
+    console.log('clicked MUAHAHAHHA I AM SECRETLY NOT A WASTE OF TIME');
   });
 
   const liButton = document.querySelector('.newli');
-  //save the involved element (or elements) as a let or const
-  liButton.addEventListener('click', () => {
-  //add a event listener to added element^
+  const userinput = document.querySelector('.userinput');
+    liButton.addEventListener('click', () => {
+    //const saveddata = localStorage.setItem('userinput.value', JSON.stringify(userinput.value));
+    //const retreiveddata = localStorage.getItem('userinput.value')
     const newli = document.createElement('LI');
-  //give js the ability to create new elements
-    const newcontent = document.createTextNode('Ive been added');
-  //add text
+    //newli.appendcontent(retreiveddata);
+    const newcontent = document.createTextNode(userinput.value);
     newli.appendChild(newcontent);
-  //append new text to the li 
     document.querySelector('.ultest').appendChild(newli);
-  //append the li to the ul
-    const makeblue = document.querySelector('.makeblue')
   });
+
+  const turnblue = document.querySelector('.level1');
+  document.querySelector('.level2');
+  document.querySelector('.level3');
+  document.querySelector('.makeblue');
 });
+
+
+// line 1: before we load anything we ensure the HTML content is loaded first to avoid errors (critical)
+// line 2: since I'm playing around I am just consolelogging to confirm it loads before anything else - in production console.logs are not encouraged.
+
+// line 9: We save the imput element as a const or a let (usually a const)
+// line 10: Event Listener is added to the new imput element
+// line 11:We then give JS the ability to create a new element in conjunction with the listener which in this case listens for a click.
+// line 12: we add the text we desire
+// line 13: we append the newtext to the li which is custom text since we call userinput.value(the value refers to custom inputs)
+// line 14: li is appended to the ul
+
+// notice that everything happens within the event listener? (the original 'click') because all functions run on that one click.
+
